5d7442993a2dca215d4ade2a74df0d46b42a2fe1
melounvitek <kontakt@vit-meloun.net>
2019-06-23 23:10:35 +0200

    Return all cards

--- src/poker_dealer/core.clj
+++ src/poker_dealer/core.clj
@@ -1,7 +1,21 @@
 (ns poker-dealer.core
   (:gen-class))
 
-(defn -main
-  "I will deal the cards."
+;(def card-values ["2" "3" "4" "5" "6" "7" "8" "9" "10" "J" "Q" "K" "A"])
+(def card-values (concat (range 2 11) ["J" "Q" "K" "A"]))
+(def card-colors ["♠"	"♥"	"♦"	"♣"])
+
+(defn color-values
+  "Returns all cards for a color."
+  [color]
+  (map #(str % color) card-values))
+
+(defn all-cards
+  "Returns all possible cards."
   []
-  (println "Cards will be printed there as the result, hopefully."))
+  (flatten (map color-values card-colors)))
+
+(defn -main
+  "Deals the cards."
+  []
+  (println (all-cards)))

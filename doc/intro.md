d68ae342f548e62672b1bb41876f6f889333309f
melounvitek <kontakt@vit-meloun.net>
2019-06-23 22:14:12 +0200

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to poker-dealer
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/poker_dealer/core.clj
@@ -0,0 +1,7 @@
+(ns poker-dealer.core
+  (:gen-class))
+
+(defn -main
+  "I will deal the cards."
+  []
+  (println "Cards will be printed there as the result, hopefully."))

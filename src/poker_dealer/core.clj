5cd51c934203da2fe9d0897c7c3756dd59d34ae2
melounvitek <kontakt@vit-meloun.net>
2019-06-26 17:01:58 +0200

    Small renaming

--- src/poker_dealer/core.clj
+++ src/poker_dealer/core.clj
@@ -10,18 +10,13 @@
   ([color denominations]
    (map #(do {:denomination %, :color color}) denominations)))
 
-(defn all-cards
+(defn deck
   "All cards in the deck."
-  ([] (all-cards (keys card-colors) card-denominations))
+  ([] (deck (keys card-colors) card-denominations))
   ([colors denominations]
    (flatten (map cards-for-color colors))))
 
-(defn shuffled-board
-  "Return shuffled cards."
-  ([] (shuffled-board (all-cards)))
-  ([cards] (shuffle cards)))
-
-(defn nice-card-print
+(defn print-card
   "Returns string with card denomination and color symbol."
   [card]
   (let [denomination (:denomination card)
@@ -38,11 +33,11 @@
   [player-index cards players-count]
   (let [card-1 (nth cards player-index)
         card-2 (nth cards (+ player-index players-count))]
-    {:player-id player-index, :cards (map nice-card-print [card-1 card-2])}))
+    {:player-id player-index, :cards (map print-card [card-1 card-2])}))
 
 (defn deal-hands
   "Assigns cards to each player; returns a map with user hands and remaning cards in the deck."
-  ([players-count] (deal-hands players-count (burn (shuffled-board))))
+  ([players-count] (deal-hands players-count (burn (shuffle (deck)))))
   ([players-count deck]
    (let [hands (map #(prepare-player-hand % deck players-count) (range 0 players-count))
         deck (burn deck (* players-count 2))]
@@ -63,7 +58,7 @@
   (let [{hands :hands, deck :deck} (deal-hands (Integer/parseInt (read-line)))]
     (println "\nPlayers hands: " hands)
     (let [{flop :flop, turn :turn, river :river} (deal-board deck)]
-      (println "\nFlop: " (clojure.string/join " " (map nice-card-print flop)))
-      (println "Turn: " (nice-card-print turn))
-      (println "River: " (nice-card-print river))
+      (println "\nFlop: " (clojure.string/join " " (map print-card flop)))
+      (println "Turn: " (print-card turn))
+      (println "River: " (print-card river))
     )))

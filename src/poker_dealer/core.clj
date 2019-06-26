83040d5a894b22a101f78d5bf7840e364a9f18b2
melounvitek <kontakt@vit-meloun.net>
2019-06-26 16:00:10 +0200

    More refactoring and code rearrangment

--- src/poker_dealer/core.clj
+++ src/poker_dealer/core.clj
@@ -16,9 +16,9 @@
   ([colors denominations]
    (flatten (map cards-for-color colors))))
 
-(defn shuffle-cards
+(defn shuffled-board
   "Return shuffled cards."
-  ([] (shuffle-cards (all-cards)))
+  ([] (shuffled-board (all-cards)))
   ([cards] (shuffle cards)))
 
 (defn nice-card-print
@@ -37,23 +37,31 @@
   [player-index cards players-count]
   (let [card-1 (nth cards player-index)
         card-2 (nth cards (+ player-index players-count))]
-    {player-index (map nice-card-print [card-1 card-2])}))
+    {:player-id player-index :cards (map nice-card-print [card-1 card-2])}))
 
 (defn deal-hands
   "Assigns cards to each player; returns a map with user hands and remaning cards in the deck."
-  [players-count deck]
-  (let [players-hands (map #(prepare-player-hand % deck players-count) (range 0 players-count))
+  ([players-count] (deal-hands players-count (burn (shuffled-board))))
+  ([players-count deck]
+   (let [hands (map #(prepare-player-hand % deck players-count) (range 0 players-count))
         deck (burn deck (* players-count 2))]
-    {:players-hands players-hands :deck deck}
-  ))
+     {:hands hands :deck deck})))
 
-(defn deal
-  ([] (deal (shuffle-cards)))
-  ([deck]
-   (println (map nice-card-print deck))
-   (let [state-after-hands-dealt (deal-hands 3 (burn deck))]
-     (println state-after-hands-dealt)
-   )))
+(defn deal-board
+  "Return flop, turn and river."
+  [deck]
+  (let [deck (burn deck)]
+    (let [flop (take 3 deck)
+         turn (nth deck 4)
+         river (nth deck 6)]
+      {:flop flop :turn turn :river river})))
 
 (defn -main []
-  (deal))
+  (println "Insert players count: ")
+  (let [{hands :hands deck :deck} (deal-hands (Integer/parseInt (read-line)))]
+    (println "\nPlayers hands: " hands)
+    (let [{flop :flop turn :turn river :river} (deal-board deck)]
+      (println "\nFlop: " (map nice-card-print flop))
+      (println "Turn: " (nice-card-print turn))
+      (println "River: " (nice-card-print river))
+    )))

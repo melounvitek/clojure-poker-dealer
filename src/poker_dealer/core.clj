24d33be78b727d26c842735af98cc1f156f82517
melounvitek <kontakt@vit-meloun.net>
2019-06-26 14:10:32 +0200

    Progress with the dealing

--- src/poker_dealer/core.clj
+++ src/poker_dealer/core.clj
@@ -24,20 +24,32 @@
 (def shuffled-cards (#(memoize shuffle-cards)))
 
 (defn nice-card-print
-  "Prints string with card denomination and color symbol."
+  "Returns string with card denomination and color symbol."
   [card]
   (let [denomination (:denomination card)
         color (get card-colors (:color card))]
-    (println (str denomination color))))
+    (str denomination color)))
+
+(defn deal-to-player
+  [player-index cards players-count]
+  (let [card-1 (nth cards player-index)
+        card-2 (nth cards (+ player-index players-count))]
+    {player-index (map nice-card-print [card-1 card-2])}))
+
+(defn deal-to-players
+  [players-count cards]
+  (let [players-hands (map #(deal-to-player % cards players-count) (range 0 players-count))
+        cards-after-deal (drop (* players-count 2) cards)]
+    {:hands players-hands :cards (map nice-card-print cards-after-deal)}
+  ))
 
 (defn deal
   ([] (deal (shuffled-cards)))
   ([cards]
-   (nice-card-print (first cards))
-   (read-line)
-   (deal (rest cards))))
+   (let [state-after-initial-deal (deal-to-players 3 (drop 1 cards))]
+     (println state-after-initial-deal)
+   )))
 
-(defn -main
-  []
-  (println (shuffled-cards))
+(defn -main []
+  (println (map nice-card-print (shuffled-cards)))
   (deal))

d93f8973acef8f9baa602129e79df3790fabbafe
melounvitek <kontakt@vit-meloun.net>
2019-07-07 23:30:14 +0200

    Add first tests

--- test/poker_dealer/core_test.clj
+++ test/poker_dealer/core_test.clj
@@ -2,6 +2,35 @@
   (:require [clojure.test :refer :all]
             [poker-dealer.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def deck-cards-count 52)
+
+(deftest deck-test
+  (let [deck (deck) colors (keys card-colors)]
+    (testing "Deck"
+      (testing "has correct cards count"
+        (is (= deck-cards-count (count deck))))
+      (testing "contains all the colors"
+        (is (= (count (set (map #(:color %) deck))) 4))))))
+
+(deftest burn-test
+  (let [deck (deck)]
+    (testing "Burn"
+      (testing "one card"
+        (is (= (burn deck) (rest deck))))
+      (testing "three cards"
+        (is (= (burn deck 3) (drop 3 deck)))))))
+
+(deftest deal-hands-test
+  (testing "Deal hands"
+    (testing "returns deck with correct cards count"
+      (testing "for 2 players"
+        (is (= (count (:deck (deal-hands 2))) (- deck-cards-count 5))))
+      (testing "for 5 players"
+        (is (= (count (:deck (deal-hands 5))) (- deck-cards-count 11)))))
+    (testing "returns correct hands count"
+      (testing "for random players count"
+        (let [players-count (rand-nth (range 1 10))]
+          (testing (str "(players count: " players-count ")")
+            (is (= (count (:hands (deal-hands players-count)))
+                   players-count))))))))
+
